c4660cda63b8e301b374b1dfed71d95827f6a7b8
Bryce <brycejensenius@gmail.com>
2024-10-10 18:11:39 -0500

    Made it so the header minimization is actually a little beneficial

--- aboutPageEventHandler.js
+++ aboutPageEventHandler.js
@@ -108,15 +108,30 @@ function randomBackgroundPosition(){
 const toggleButton = document.getElementById('toggleNav');
 const navItems = document.getElementById('navItems');
 const headNavigation = document.getElementById('headNavElem');
+const header = document.getElementById('heading');
+let headingToggleState = false;
 function handleNavVisibility() {
     if (window.innerWidth > 800) {
         navItems.classList.add('show');  // Always show nav items on larger screens
         headNavigation.classList.add('justify-content-center');
         toggleButton.style.display = 'none'; // Hide the toggle button
+        header.style.padding = '15px 0';
+        header.style.marginBottom = '15px';
     } else {
         navItems.classList.toggle('displayInRows');
         toggleButton.addEventListener('click', () => {
-            navItems.classList.toggle('show');
+
+            if(headingToggleState){ // Toggled go untoggle and make smaller
+                navItems.classList.toggle('show');
+                header.style.padding = '3px 10px';
+                header.style.marginBottom = '3px';
+                headingToggleState = false;
+            }else{
+                navItems.classList.toggle('show');
+                header.style.padding = '15px 10px';
+                header.style.marginBottom = '15px';
+                headingToggleState = true;
+            }
         });
     }
 }
