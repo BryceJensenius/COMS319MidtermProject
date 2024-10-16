d68b2ae5ff3bafcdcf4b6c16a62bde20a511cf45
Bryce <brycejensenius@gmail.com>
2024-10-16 10:56:35 -0500

    Fixed header, scaling on about page is fixed

--- navFunctionality.js
+++ navFunctionality.js
@@ -5,33 +5,38 @@ const headNavigation = document.getElementById('headNavElem');
 const header = document.getElementById('heading');
 let headingToggleState = false;
 
+toggleButton.addEventListener('click', () => {
+    if(!headingToggleState){ // Toggled go untoggle and make smaller
+        navItems.classList.add('show');
+        header.style.padding = '15px 10px';
+        header.style.marginBottom = '3px';
+        headingToggleState = true;
+    }else{
+        navItems.classList.remove('show');
+        header.style.padding = '3px 10px';
+        header.style.marginBottom = '15px';
+        headingToggleState = false;
+    }
+});
+
 function handleNavVisibility() {
     if (window.innerWidth > 800) {
         navItems.classList.add('show');  // Always show nav items on larger screens
         headNavigation.classList.add('justify-content-center');
-        toggleButton.style.display = 'none'; // Hide the toggle button
         header.style.padding = '15px 0';
         header.style.marginBottom = '15px';
+        toggleButton.style.display = 'none';
     } else {
-        navItems.classList.toggle('displayInRows');
-        toggleButton.addEventListener('click', () => {
-
-            if(headingToggleState){ // Toggled go untoggle and make smaller
-                navItems.classList.toggle('show');
-                header.style.padding = '3px 10px';
-                header.style.marginBottom = '3px';
-                headingToggleState = false;
-            }else{
-                navItems.classList.toggle('show');
-                header.style.padding = '15px 10px';
-                header.style.marginBottom = '15px';
-                headingToggleState = true;
-            }
-        });
+        toggleButton.style.display = '';
+        headNavigation.classList.remove('justify-content-center');
+        if(navItems.classList.contains('show')){ // for larger screen getting shrunk
+            navItems.classList.remove('show');
+            header.style.padding = '3px 10px';
+            header.style.marginBottom = '15px';
+            headingToggleState = false;
+        }
     }
 }
 
+window.addEventListener('resize', handleNavVisibility);
 handleNavVisibility();
-
-console.log("JS Loaded\n\n");
-window.onload = getAboutCards;
